ff358bbb9fd1b85b4ff01869ba7b0d5d4e3422df
Aleksey Serdtsev <sng.player@gmail.com>
2022-06-09 10:40:55 +0300

    Added UserBar fetch user error handling

--- client/libs/components/user-bar/src/lib/user-bar.tsx
+++ client/libs/components/user-bar/src/lib/user-bar.tsx
@@ -2,9 +2,10 @@ import { fetchUser } from "@pg/api";
 import styles from "./styles.module.css";
 
 export const UserBar = () => {
-    const [loading, user] = fetchUser();
+    const [loading, error, user] = fetchUser();
 
     if (loading) return <div>Loading...</div>;
+    if (error) return <div>Unauthorized</div>;
     if (!user) return null;
     return (
         <div className={styles.bar}>
